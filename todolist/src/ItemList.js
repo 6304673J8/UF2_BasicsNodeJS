3c4fdca92ef89f91b6921efd78a1221890afbe6b
6304673J8 <petergeorghe.julca@enti.cat>
2021-04-16 14:30:28 +0200

    Item List-Delete Button Added

--- todolist/src/ItemList.js
+++ todolist/src/ItemList.js
@@ -3,11 +3,20 @@ import React, { Component } from 'react';
 //import './TodoList.css';
 
 class ItemList extends Component {
+	constructor (props){
+		super(props);
+
+		this.eraseItem = this.eraseItem.bind(this);
+	}
+
+	eraseItem(){
+		this.props.parentRemove(this.props.id_item);
+	}
 	render(){ 
 		return (
 			<li>
 				{this.props.item}
-				<button>X {this.props.eraseItem}</button>
+				<button className="delete" onClick={this.eraseItem}>X</button>
 			</li>
 		);
 	}
--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -5,12 +5,11 @@ class TodoList extends Component {
 	constructor (props) {
 		super(props);
 
-		let contador = 1;
-		let todo_tasks= [];
 		this.state = {
-			items : todo_tasks
+			items : []
 		};
 
+		this.last_id = 0;
 		this.addItem = this.addItem.bind(this);
 		this.eraseItem = this.eraseItem.bind(this);
 	}
@@ -19,42 +18,54 @@ class TodoList extends Component {
 		e.preventDefault();
 
 		let text_v = document.getElementById("text-task").value;
-				
-		this.state.items.push({id:0, item:text_v});
+		
+		document.getElementById("text-task").value = "";
+		document.getElementById("text-task").value.focus;
+		
+		this.last_id++;
+
+		this.state.items.push({id: this.last_id, item:text_v});
+		console.log(this.state.items);
+
 		this.setState({
 			items: this.state.items
 		});
 	}
 
-	eraseItem(e) {
-		
-		let text_v = document.getElementById("text-task").value;
-		this.state.items.splice({id:0, item:text_v});
+	eraseItem(id_item) {
+		for (let i=0; i < this.state.items.length; i++){
+			if(this.state.items[i].id === id_item){
+				this.state.items.splice(i, 1);
+				break;
+			}
+		}
+
 		this.setState({
 			items: this.state.items
-		});	
+		});
 	}
 
 	render() {
 		let lista = this.state.items.map( (todo_item) => {
-			return (<ItemList item={todo_item.item}/>);
+			return (<ItemList item={todo_item.item}
+					id_item={todo_item.id}
+					parentRemove={this.eraseItem} />);
 		});
-
-		const listSize={
-			textAlign: "center"
-		}
+		
 		return (
-			<div style={listSize}>
-				<h2>Testerino tino tino, co-co-ding</h2>
+			<div>
+        <p>Num Items: NUM</p>
 
+				<form onSubmit={this.addItem}>
+					<p><input type="text" id="text-task" />
+					<button type="submit">Add</button></p>
+				</form>
+				
 				<ul>
 					{lista}
 				</ul>
 
-				<form onSubmit={this.addItem}>
-					<p><input type="text" id="text-task" /></p>
-					<p><button type="submit">Add</button></p>
-				</form>
+
 			</div>
 		);
 	}
